0c39458c611a557f501b83572bd6edc5ab5cd917
Milton Morales <10735359+miltonjmc@users.noreply.github.com>
2024-03-16 10:51:57 -0400

    Add files via upload

---
+++ Dex3/Dex3/Dex3App.swift
@@ -0,0 +1,20 @@
+//
+//  Dex3App.swift
+//  Dex3
+//
+//  Created by Milton Morales on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Dex3App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Dex3/Dex3Widget/Dex3WidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  Dex3WidgetBundle.swift
+//  Dex3Widget
+//
+//  Created by Milton Morales on 3/15/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct Dex3WidgetBundle: WidgetBundle {
+    var body: some Widget {
+        Dex3Widget()
+        Dex3WidgetLiveActivity()
+    }
+}
